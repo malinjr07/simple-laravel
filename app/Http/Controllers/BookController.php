5cf6ec694ed4457d53444e43d63276a4d7c78c6c
marufc9 <Mosaddik.AliMaruf@c9.com.au>
2025-05-08 13:10:25 +0600

    Basic API route init

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json([
+            'message' => 'Hello World',
+            'posts' => [
+                ['id' => 1, 'title' => 'Post 1'],
+                ['id' => 2, 'title' => 'Post 2'],
+            ],
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
